d98c787ee1c5b34126cdd7df99e7430e56bf388a
Akiomi Kamakura <akiomik@gmail.com>
2021-03-08 00:24:49 +0900

    Add image package

--- cmd/root.go
+++ cmd/root.go
@@ -3,15 +3,13 @@ package cmd
 import (
 	"fmt"
 	"image"
-	"image/color"
 	"image/color/palette"
 	"image/gif"
-	"math"
 	"os"
 
 	"github.com/akiomik/shiomi/audio"
 	"github.com/akiomik/shiomi/config"
-	"github.com/fogleman/gg"
+	simage "github.com/akiomik/shiomi/image"
 	"github.com/spf13/cobra"
 )
 
@@ -23,6 +21,8 @@ var (
 	rate       uint
 	width      int
 	height     int
+	bgColor    string
+	fgColor    string
 )
 
 var rootCmd = &cobra.Command{
@@ -65,14 +65,13 @@ var rootCmd = &cobra.Command{
 
 		var images []*image.Paletted
 		var delays []int
-
-		bgColor := color.RGBA{0x00, 0x00, 0x00, 0xff}
-		fgColor := color.RGBA{0x00, 0xff, 0x00, 0xff}
 		colorPalette := palette.Plan9
 
-		heightf := float64(height)
-		widthf := float64(width)
-		xScale := widthf / a.NumSamplesPerWindow()
+		simg, err := simage.NewSignalImage(uint(width), uint(height), bgColor, fgColor, 0.8)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
 
 		readCycles := a.ReadCycles()
 		for audioData := range readCycles {
@@ -82,32 +81,11 @@ var rootCmd = &cobra.Command{
 				os.Exit(1)
 			}
 
-			dc := gg.NewContext(width, height)
-			dc.SetColor(bgColor)
-			dc.DrawRectangle(0, 0, widthf, heightf)
-			dc.Fill()
-			dc.SetColor(fgColor)
-			dc.SetLineWidth(0.8)
+			simg.DrawSignal(samples)
+			pimg := simg.ConvertToPaletted(colorPalette)
+			simg.Clear()
 
-			for j, sample := range samples[:len(samples)-1] {
-				x1 := math.Round(float64(j) * xScale)
-				y1 := heightf - math.Round(float64(sample)*heightf)
-				x2 := math.Round(float64(j+1) * xScale)
-				y2 := heightf - math.Round(float64(audioData.Samples[j+1])*heightf)
-				dc.DrawLine(x1, y1, x2, y2)
-			}
-
-			dc.Stroke()
-
-			img := image.NewPaletted(dc.Image().Bounds(), colorPalette)
-			bounds := img.Bounds()
-			for x := bounds.Min.X; x < bounds.Max.X; x++ {
-				for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-					img.Set(x, y, dc.Image().At(x, y))
-				}
-			}
-
-			images = append(images, img)
+			images = append(images, pimg)
 			delays = append(delays, 0)
 		}
 
@@ -134,9 +112,12 @@ func init() {
 	rootCmd.Flags().UintVarP(&freq, "freq", "f", 1000, "The frequency of an input file [Hz]")
 	rootCmd.Flags().UintVarP(&windowSize, "num", "n", 3, "An output number of cycles")
 	rootCmd.Flags().UintVarP(&rate, "rate", "r", 10, "A subsampling rate for output")
-	rootCmd.Flags().IntVarP(&width, "width", "", 640, "A width of an output file")
-	rootCmd.Flags().IntVarP(&height, "height", "", 480, "A height of an output file")
+	rootCmd.Flags().IntVarP(&width, "width", "", 640, "A width of an output file [px]")
+	rootCmd.Flags().IntVarP(&height, "height", "", 480, "A height of an output file [px]")
+	rootCmd.Flags().StringVarP(&bgColor, "bg", "", "#000000", "A background color for output")
+	rootCmd.Flags().StringVarP(&fgColor, "fg", "", "#00ff00", "A foreground color for output")
 	rootCmd.MarkFlagRequired("input")
+	rootCmd.MarkFlagRequired("output")
 }
 
 func Execute() {
---
+++ image/image.go
@@ -0,0 +1,81 @@
+package image
+
+import (
+	"errors"
+	"image"
+	"image/color"
+	"math"
+
+	"github.com/fogleman/gg"
+)
+
+type SignalImage struct {
+	Width     uint
+	Height    uint
+	BgColor   string
+	FgColor   string
+	LineWidth float32
+	context   *gg.Context
+}
+
+func NewSignalImage(width uint, height uint, bgColor string, fgColor string, lineWidth float32) (*SignalImage, error) {
+	if width <= 0 || height <= 0 {
+		return nil, errors.New("width and height must be > 0")
+	}
+
+	context := gg.NewContext(int(width), int(height))
+	return &SignalImage{Width: width, Height: height, BgColor: bgColor, FgColor: fgColor, LineWidth: lineWidth, context: context}, nil
+}
+
+func (simg *SignalImage) DrawBackground() {
+	simg.context.SetHexColor(simg.BgColor)
+	simg.context.DrawRectangle(0, 0, float64(simg.Width), float64(simg.Height))
+	simg.context.Fill()
+}
+
+func (simg *SignalImage) DrawPolyLine(points []gg.Point) {
+	simg.context.SetHexColor(simg.FgColor)
+	simg.context.SetLineWidth(0.8)
+
+	for i, p1 := range points[:len(points)-1] {
+		p2 := points[i+1]
+		simg.context.DrawLine(p1.X, p1.Y, p2.X, p2.Y)
+	}
+
+	simg.context.Stroke()
+}
+
+func (simg *SignalImage) DrawSignal(data []float32) {
+	widthf := float64(simg.Width)
+	heightf := float64(simg.Height)
+	xScale := widthf / float64(len(data))
+
+	simg.DrawBackground()
+
+	var points = make([]gg.Point, len(data))
+	for i, d := range data {
+		x := math.Round(float64(i) * xScale)
+		y := heightf - math.Round(float64(d)*heightf)
+		points[i] = gg.Point{X: x, Y: y}
+	}
+
+	simg.DrawPolyLine(points)
+}
+
+func (simg *SignalImage) Clear() {
+	simg.context.Clear()
+}
+
+func (simg *SignalImage) ConvertToPaletted(colorPalette color.Palette) *image.Paletted {
+	img := simg.context.Image()
+	bounds := img.Bounds()
+	pimg := image.NewPaletted(bounds, colorPalette)
+
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			pimg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return pimg
+}
